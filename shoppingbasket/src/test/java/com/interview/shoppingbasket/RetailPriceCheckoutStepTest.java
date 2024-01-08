520b5aed4a5348b6c1dbd40b53998d5add0b6437
joao7cruz97 <156006818+joao7cruz97@users.noreply.github.com>
2024-01-08 18:42:01 +0000

    Update RetailPriceCheckoutStepTest.java

--- shoppingbasket/src/test/java/com/interview/shoppingbasket/RetailPriceCheckoutStepTest.java
+++ shoppingbasket/src/test/java/com/interview/shoppingbasket/RetailPriceCheckoutStepTest.java
@@ -1,56 +1,49 @@
 package com.interview.shoppingbasket;
 
-import java.util.List;
+import static org.mockito.Mockito.when;
 
-public class RetailPriceCheckoutStep implements CheckoutStep {
-    private PricingService pricingService;
-    private double retailTotal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-    public RetailPriceCheckoutStep(PricingService pricingService) {
-        this.pricingService = pricingService;
+public class RetailPriceCheckoutStepTest {
+
+    PricingService pricingService;
+    CheckoutContext checkoutContext;
+    Basket basket;
+
+    @BeforeEach
+    void setup() {
+        pricingService = Mockito.mock(PricingService.class);
+        checkoutContext = Mockito.mock(CheckoutContext.class);
+        basket = new Basket();
+
+        when(checkoutContext.getBasket()).thenReturn(basket);
     }
 
-    @Override
-    public void execute(CheckoutContext checkoutContext) {
-        Basket basket = checkoutContext.getBasket();
-        List<Promotion> promotions = checkoutContext.getPromotions();
-        retailTotal = 0.0;
+    @Test
+    void setPriceZeroForEmptyBasket() {
 
-        for (BasketItem basketItem: basket.getItems()) {
-            int quantity = basketItem.getQuantity();
-            double price = pricingService.getPrice(basketItem.getProductCode());
-            boolean promotionApplied = false;
-            for(Promotion promotion : promotions){
-                if(promotion.getProductsCodes().contains(basketItem.getProductCode())){
-                    price = applyPromotion(promotion,basketItem,price);
-                    retailTotal += price;
-                    promotionApplied = true;
-                    break;
-                }
-            }
-            if(!promotionApplied) {
-                basketItem.setProductRetailPrice(price);
-                retailTotal += quantity * price;
-            }
-        }
+        RetailPriceCheckoutStep retailPriceCheckoutStep = new RetailPriceCheckoutStep(pricingService);
 
-        checkoutContext.setRetailPriceTotal(retailTotal);
+        retailPriceCheckoutStep.execute(checkoutContext);
+
+        Mockito.verify(checkoutContext).setRetailPriceTotal(0.0);
     }
 
-    private double applyPromotion(Promotion promotion, BasketItem basketItem, double price) {
-        switch (promotion.getPromotionType()){
-            case "2FOR1":
-                int free = basketItem.getQuantity() / 2;
-                return ((basketItem.getQuantity() - free) * price);
-            case "50":
-                double discount = (basketItem.getQuantity()) * price * 0.5;
-                return discount;
-            case "10":
-                double discount2 = (basketItem.getQuantity()) *  price * 0.1;
-                return (price * basketItem.getQuantity()) - discount2;
-            default:
-                return price;
-        }
+    @Test
+    void setPriceOfProductToBasketItem() {
+
+        basket.add("product1", "myproduct1", 10);
+        basket.add("product2", "myproduct2", 10);
+
+        when(pricingService.getPrice("product1")).thenReturn(3.99);
+        when(pricingService.getPrice("product2")).thenReturn(2.0);
+        RetailPriceCheckoutStep retailPriceCheckoutStep = new RetailPriceCheckoutStep(pricingService);
+
+        retailPriceCheckoutStep.execute(checkoutContext);
+        Mockito.verify(checkoutContext).setRetailPriceTotal(3.99*10+2*10);
+
     }
 
 }
